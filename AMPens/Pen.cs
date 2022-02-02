28d6f9fdb1e91e03c11f0fc2be5aa545088088f1
MUKILTEO\1610424 <1610424@mukilteo.wednet.edu>
2022-02-02 08:40:37 -0800

    Add project files.

---
+++ AMPens/Pen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMPens
+{
+    public class Pen
+    {
+
+        //Properties
+        public string Name;
+        public float CasingThickness;
+        public string InkColor;
+        public bool HasCap;
+        public int NumberOfInkColors;
+        public float InkLevelPercent;
+        public bool HasLight;
+        public bool IsUsable;
+
+        //Constructors
+        public Pen()
+        {
+
+        }
+
+        public Pen(string name, float casingThickness, string inkColor, bool hasCap, int numberOfInkColors, float inkLevelPercent)
+        {
+            Name = name;
+            CasingThickness = casingThickness;
+            InkColor = inkColor;
+            HasCap = hasCap;
+            NumberOfInkColors = numberOfInkColors;
+            InkLevelPercent = inkLevelPercent;
+        }
+
+        public Pen(bool hasLight, bool isUsable)
+        {
+            HasLight = hasLight;
+            IsUsable = isUsable;
+        }
+
+
+
+        //Methods
+        public void Describe()
+        {
+            Console.WriteLine("\tIs Usable: " + IsUsable);
+            if(IsUsable = true)
+            {
+                Console.WriteLine("Please don't break me");
+            }
+            Console.WriteLine("\tHas Light: " + HasLight);
+            Console.WriteLine("\nInformation about " + Name);
+            Console.WriteLine("\tInk Color: " + InkColor);
+            Console.WriteLine("\tHas Cap?: " + HasCap);
+            Console.WriteLine("\tInk Level%: " + InkLevelPercent);
+            if(InkLevelPercent < 10)
+            {
+                Console.WriteLine("Needs A Refill!!");
+            }
+        }
+
+
+    }
+}
---
+++ AMPens/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AMPens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Pen CaseysAmericanPen = new Pen(); //Instantiated a Pen!
+            CaseysAmericanPen.Name = "Casey's Pen from America in 2004";
+            CaseysAmericanPen.InkColor = "Red";
+            CaseysAmericanPen.HasCap = true;
+            CaseysAmericanPen.InkLevelPercent = 0.0f;
+            CaseysAmericanPen.NumberOfInkColors = 1;
+            CaseysAmericanPen.CasingThickness = 0.25f;
+            CaseysAmericanPen.HasLight = true;
+            CaseysAmericanPen.IsUsable = true;
+
+            //Console.WriteLine(CaseysAmericanPen.Name + " has " + CaseysAmericanPen + " ink. ");
+
+            Pen Bic4C = new Pen("The Bic 4 Color Geek Pen", 0.4f, "Multiple", false, 4, 100);
+            //Console.WriteLine(Bic4C.Name + " has " + Bic4C.InkColor + " ink. ");
+
+
+            CaseysAmericanPen.Describe();
+            Bic4C.Describe();
+
+
+        }
+    }
+}
